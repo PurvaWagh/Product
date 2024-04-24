da6d395af6d65902059ae83b45681d65de9cd827
Purva Wagh <89512238+PurvaWagh@users.noreply.github.com>
2024-04-24 18:44:54 +0530

    Delete ProductApp/ProductApp directory

--- ProductApp/ProductApp/src/main/java/com/example/ProductApp/Repo/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ProductApp.Repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.ProductApp.Entity.Product;
-
-public interface ProductRepository extends JpaRepository<Product,String>{
-
-}
